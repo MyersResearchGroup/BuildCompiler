bc95f5f676a91959a2ac077fc6bf7688747c49ed
Gonzalo Vidal <35148159+Gonza10V@users.noreply.github.com>
2026-05-05 22:07:40 -0600

    Add SBOL AssemblyService wrapper and contracts

--- src/buildcompiler/sbol/__init__.py
+++ src/buildcompiler/sbol/__init__.py
@@ -1,5 +1,12 @@
 """SBOL package exports for clean architecture contracts."""
 
+from .assembly import AssemblyJob, AssemblySbolResult, AssemblyService
 from .resolver import PullPolicy, SbolResolver
 
-__all__ = ["PullPolicy", "SbolResolver"]
+__all__ = [
+    "AssemblyJob",
+    "AssemblySbolResult",
+    "AssemblyService",
+    "PullPolicy",
+    "SbolResolver",
+]
