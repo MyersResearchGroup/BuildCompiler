3fdcf3e0c9fa2f7a844af00eaa2fdc58bfa3d8b6
Gonzalo Vidal <35148159+Gonza10V@users.noreply.github.com>
2026-05-06 00:04:27 -0600

    Implement lvl2 assembly stage with route blocking promotion

--- src/buildcompiler/stages/__init__.py
+++ src/buildcompiler/stages/__init__.py
@@ -1,6 +1,7 @@
 """Stage exports."""
 
 from .assembly_lvl1 import AssemblyLvl1Stage
+from .assembly_lvl2 import AssemblyLvl2Stage
 from .domestication import DomesticationStage
 
-__all__ = ["AssemblyLvl1Stage", "DomesticationStage"]
+__all__ = ["AssemblyLvl1Stage", "AssemblyLvl2Stage", "DomesticationStage"]
